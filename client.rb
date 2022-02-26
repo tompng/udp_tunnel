15c668784238081669036ebb071508ced3d7ab1c
tompng <tomoyapenguin@gmail.com>
2022-02-27 01:55:56 +0900

    ack connect

--- client.rb
+++ client.rb
@@ -1,13 +1,50 @@
 require_relative 'stun'
 require_relative 'connection'
 socket = UDPSocket.open
+socket.bind '0.0.0.0', 0
 # stun = Stun.new socket
 # global_ip, global_port = stun.get_ip_port
 # p [global_ip, global_port]
 
 local_port = socket.addr[1]
+p local_port
+
 server = ConnectionManager.new socket
 
-loop do
-  server.accept
+connections = []
+conn = nil
+Thread.new do
+  loop do
+    conn = server.accept
+    p :accepted
+    connections << conn
+    Thread.new {
+      loop do
+        type, data = conn.read
+        p [type, data]
+        if data&.start_with? 'echo'
+          conn.send_data 'ok' + data[4..]
+        end
+        if type == :close
+          connections -= [conn]
+          break
+        end
+      end
+    }
+  end
 end
+
+port_file = 'port.txt'
+case ARGV[0]
+when 'a'
+  File.write port_file, local_port.to_s
+  at_exit { File.unlink port_file }
+when 'b'
+  sleep 0.1 until File.exist? port_file
+  port = File.read(port_file).to_i
+  conn = server.connect '127.0.0.1', port
+else
+  define_singleton_method(:connect){ server.connect '127.0.0.1', _1 }
+end
+
+binding.irb
--- connection.rb
+++ connection.rb
@@ -16,21 +16,22 @@ class ConnectionManager
       ip = addr[3]
       port = addr[1]
       key = [ip, port]
-      type = data.ord
+      type_id = data.ord
       msg = data[1..]
-      case type
-      when Connection::TYPE_REQ
-        find_or_accept_connection(ip, port, create: true)&.send_ack
-      when Connection::TYPE_ACK
-        rid, ridx, sid, scnt = msg.unpack 'NNN'
-        find_or_accept_connection(ip, port, create: true)&.handle_ack rid, ridx, sid, scnt
-      when Connection::TYPE_DATA
+      p [:recv, Connection::TYPES[type_id], msg]
+      case Connection::TYPES[type_id]
+      when :req
+        find_or_accept_connection(ip, port)&.send_ack
+      when :ack
+        rid, ridx, sid, scnt = msg.unpack 'NNNN'
+        @connections[key].handle_ack rid, ridx, sid, scnt
+      when :data
         connection_id, idx = msg.unpack 'NN'
-        @connections[key]&.handle_data connection_id, idx, msg[4..]
-      when Connection::TYPE_RESEND
-        connection_id, idxs = msg.unpack 'N*'
+        @connections[key]&.handle_data connection_id, idx, msg[8..]
+      when :resend
+        connection_id, *idxs = msg.unpack 'N*'
         @connections[key]&.handle_resend connection_id, idxs
-      when Connection::TYPE_CLOSE
+      when :close
         @connections[key]&.handle_close
         @connections.delete key
       end
@@ -38,7 +39,7 @@ class ConnectionManager
   end
 
   def find_or_accept_connection(ip, port)
-    key = ip, port
+    key = [ip, port]
     conn = @connections[key]
     if conn.nil? && accept_new_connection?
       conn = @connections[key] = Connection.new self, ip, port
@@ -48,6 +49,7 @@ class ConnectionManager
   end
 
   def send_raw(data, ip, port)
+    p [:raw, data]
     @socket.send data, 0, ip, port
   end
 
@@ -56,20 +58,28 @@ class ConnectionManager
   end
 
   def connect(ip, port)
-    conn = @connections[[ip, port]] ||= Connection.new socket, self, ip, port
-    conn.send_req
+    conn = @connections[[ip, port]]
+    unless conn
+      conn = @connections[[ip, port]] = Connection.new self, ip, port
+      conn.send_req
+    end
+    conn
   end
 
   def run_tick
-    @connections.each_value { _1.tick }
+    loop do
+      @connections.each_value { _1.tick }
+      sleep 1
+    end
+  end
+
+  def remove(ip, port)
+    @connections.delete [ip, port]
   end
 end
 
 class Connection
-  TYPE_REQ = 0
-  TYPE_ACK = 1
-  TYPE_DATA = 2
-  TYPE_RESEND = 3
+  TYPES = %i[req ack data resend close]
 
   def initialize(manager, ip, port)
     @manager = manager
@@ -77,12 +87,12 @@ class Connection
     @ip = ip
     @port = port
     @event_queue = Thread::Queue.new
-    @state = :connecting
+    @status = :connecting
     @initialized_at = @last_recv_ack = Time.now
     @last_send_ack = nil
     @terminated_recv_connection_id = nil
     reset_send_connection
-    reset_recv_connection nil
+    reset_recv_connection 0
   end
 
   def reset_send_connection
@@ -101,14 +111,14 @@ class Connection
   def close
     return if closed?
     handle_close
-    socket_send TYPE_CLOSE
+    socket_send :close
   end
 
   def handle_close
     return if closed?
-    @state = :closed
+    @status = :closed
     trigger :close
-    @manager.delete [@ip, @port]
+    @manager.remove @ip, @port
   end
 
   def trigger(type, message = nil)
@@ -120,51 +130,51 @@ class Connection
   end
 
   def send_data(message)
-    message.each_slice 1024 do |msg|
+    message.chars.each_slice(1024).map(&:join).map do |msg|
       idx = @send_idx + @send_buffer.size
       @send_buffer << msg
-      socket_send TYPE_DATA, idx, msg
+      socket_send :data, @send_connection_id, idx, msg
     end
   end
 
-  def closed? = @state == :closed
-  def connecting? = @state == :connecting
-  def connected? = @state == :connected
+  def closed? = @status == :closed
+  def connecting? = @status == :connecting
+  def connected? = @status == :connected
 
   def send_close
     return if closed?
-    socket_send TYPE_CLOSE
+    socket_send :close
   end
 
   def send_req
     @last_send_req = Time.now
-    socket_send TYPE_REQ
+    socket_send :req
   end
 
   def tick
     current = Time.now
     if connecting?
-      if @initialized_at - current > 30 * 1000
+      if current - @initialized_at > 30
         handle_close
-      elsif @last_send_req.nil? || @last_send_req - current > 1000
+      elsif @last_send_req.nil? || current - @last_send_req > 1
         send_req
       end
     elsif connected?
-      send_ack if @last_send_ack - current > 5000
+      send_ack if @last_send_ack.nil? || current - @last_send_ack > 5
     end
   end
 
   def send_ack
     @last_send_ack = Time.now
-    socket_send TYPE_ACK, @connection_id, @recv_idx, @send_idx + @send_buffer.size
+    socket_send :ack, @recv_connection_id, @recv_idx, @send_connection_id, @send_idx + @send_buffer.size
   end
 
   def handle_ack(send_id, reached_idx, recv_id, remote_sent_count)
     if connecting?
       trigger :open
-      @state = :connected
+      @status = :connected
     end
-    return if send_id != @send_connection_id
+    return if send_id != @send_connection_id && reached_idx != 0
     return if recv_id == @terminated_recv_connection_id
     reset_recv_connection recv_id if recv_id != @recv_connection_id
     if reached_idx >= @send_idx
@@ -174,7 +184,8 @@ class Connection
       trigger :reset
     end
     @send_idx = reached_idx
-    @recv_buffer[remote_sent_count - 1 - @recv_idx] ||= nil if remote_sent_count > 0
+    i = remote_sent_count - 1 - @recv_idx
+    @recv_buffer[i] ||= nil if i >= 0
     @last_recv_ack = Time.now
     request_resend
   end
@@ -194,7 +205,7 @@ class Connection
     return if send_id != @send_connection_id
     idxs.each do |idx|
       msg = @send_buffer[idx - @send_idx]
-      socket_send TYPE_DATA, @connection_id, idx, msg
+      socket_send :data, @send_connection_id, idx, msg
     end
   end
 
@@ -208,12 +219,15 @@ class Connection
     ids = @unreceiveds.keys.select do |id|
       current - @unreceiveds[id] > timeout
     end
+    return if ids.empty?
     ids.each { @unreceiveds[_1] = current + timeout }
-    socket_send TYPE_RESEND, @recv_connection_id, ids
+    socket_send :resend, @recv_connection_id, ids
   end
 
   def socket_send(type, *data)
-    sdata = data.flatten.map { _1.is_a?(Number) ? [_1].pack('N') : _1 }.join
-    @manager.send_raw type.chr + sdata, 0, @ip, @port
+    p [:socket_send, type, data]
+    sdata = data.flatten.map { _1.is_a?(Numeric) ? [_1].pack('N') : _1 }.join
+    type_id = TYPES.index type
+    @manager.send_raw type_id.chr + sdata, @ip, @port
   end
 end
