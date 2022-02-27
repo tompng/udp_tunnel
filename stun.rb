c20f4ea743d1e9ea8f52df64b4ba951ec0792a60
tompng <tomoyapenguin@gmail.com>
2022-02-27 16:01:18 +0900

    stun magic cookie fix

--- stun.rb
+++ stun.rb
@@ -1,5 +1,6 @@
 require 'securerandom'
 require 'socket'
+require 'timeout'
 
 class Stun
   def initialize(socket)
@@ -15,30 +16,49 @@ class Stun
   ]
 
   def get_ip_port
-    results = ADDRS.sample(2).map do |host, port|
+    servers = ADDRS.sample 2
+    results = servers.map do |host, port|
       test host, port
     end
     if results.uniq.size != 1
+      p servers.zip(results)
       raise 'Symmetric NAT. unsupported'
     end
     results[0]
   end
 
-  def test(host, port)
-    id = SecureRandom.random_bytes 16
-    @socket.send [1, 0].pack('nn') + id, 0, host, port
-    data, _addr = @socket.recvfrom 1024
-    raise 'invalid response from stun server' unless id == data[4...20]
+  def test(host, port, legacy = false)
+    magic_cookie = 0x2112A442
+    magic_cookie = rand(0xffffffff) if legacy
+    magic_cookie_transaction_id = [magic_cookie].pack('N') + SecureRandom.random_bytes(12)
+    @socket.send [1, 0].pack('nn') + magic_cookie_transaction_id, 0, host, port
+    data = Timeout.timeout 5 do
+      loop do
+        response, _addr = @socket.recvfrom 1024
+        break response if magic_cookie_transaction_id == response[4...20]
+      end
+    end
     attr_chars = data[20..].chars
     attrs = {}
     until attr_chars.empty?
       type, size = attr_chars.shift(4).join.unpack('nn')
-      value = attr_chars.shift(size).join
+      value = attr_chars.shift((size + 3) / 4 * 4).take(size).join
       attrs[type] = value
     end
-    value = attrs[1]
-    global_port = value[2, 2].unpack1 'n'
-    global_ip = value[4..].bytes
-    [global_ip, global_port]
+    # 1: MAPPED-ADDRESS, 2: XOR-MAPPED-ADDRESS
+    value = attrs[1] || attrs[32]
+    return unless value
+    xor = attrs[1] ? [0] : magic_cookie_transaction_id.unpack('N*')
+    family, xport, *xip = value.unpack 'nnN*'
+    port = xport ^ (xor[0] >> 16)
+    ip = xip.zip(xor.cycle).map { _1 ^ _2 }.pack('N*').unpack 'C*'
+    case family
+    when 1 # IPv4
+      [ip.join('.'), port]
+    when 2 # IPv6
+      [ip.join(':'), port]
+    end
+  rescue Timeout::Error
+    nil
   end
 end
